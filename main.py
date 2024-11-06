1e0dc50ed6ab3c05560fa3522d3e6021568aa654
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2024-11-05 22:01:22 -0800

    Add garbling features and synonym + adjective

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+import json
+import os.path
 import subprocess
 import sys
 
@@ -29,13 +31,12 @@ GARBLE_METHODS = IndexedOrderedDict(
 
 PROMPT = "garbler >> "
 
-input_method = None
 garble_method = None
-output_method = None
-plaintext = []
+plaintext = None
+garbled_text = ""
 
 input_handle = None
-output_handle = None
+output_file = None
 output_exists = False
 
 garble_ready = False
@@ -54,9 +55,9 @@ using the “Get help” option.
 
 
 def show_menu():
-    global input_method
+    global plaintext
     global garble_method
-    global output_method
+    global output_file
     # Some of these symbols may not show up in VSCode depending on the font you're using.
 
     print("")
@@ -65,8 +66,8 @@ def show_menu():
     print("Choose an option by typing its number.")
     print("")
 
-    if input_method is not None:
-        print(f"1) Replace input text - ℹ️ currently set: {input_method} ✅")
+    if plaintext is not None:
+        print(f"1) Replace input text - ℹ️ currently set: Standard input ✅")
     else:
         print(f"1) Add input text to be garbled")
 
@@ -75,8 +76,10 @@ def show_menu():
     else:
         print(f"2) Choose garbling method")
 
-    if output_method is not None:
-        print(f"3) Update output method - ℹ️ currently selected: {output_method} ✅")
+    if output_file is not None:
+        print(
+            f"3) Update output method - ℹ️ currently selected: {'Standard output' if output_file is None else output_file} ✅"
+        )
         if output_exists:
             print("    (⚠️ warning: this file will be overwritten!)")
     else:
@@ -134,55 +137,51 @@ exists, {color.BOLD}it will be overwritten{color.RESET}. Please choose wisely!
 
 
 def update_garble_status():
-    global input_method
+    global plaintext
     global garble_method
-    global output_method
+    global output_file
     global garble_ready
 
-    if (
-        input_method is not None
-        and garble_method is not None
-        and output_method is not None
-    ):
+    if plaintext is not None and garble_method is not None and output_file is not None:
         garble_ready = True
     else:
         garble_ready = False
 
 
 def clear_garble_status():
-    global input_method
-    global garble_method
-    global output_method
-    global garble_ready
     global plaintext
+    global garble_method
+    global output_file
+    global garble_ready
 
-    input_method = None
+    plaintext = None
     garble_method = None
-    output_method = None
+    output_file = None
     garble_ready = False
-    plaintext = []
 
 
 while True:
     show_menu()
     cmd = input(PROMPT)
     print("")
+
     if cmd == "1":
+        plaintext = ""
         print(
             f"Enter the text to be garbled below. End your input with {color.BOLD}ctrl+d{color.RESET}."
         )
-        # Get multiline input until we get EOF - taken from
+        # Get multiline input until we get EOF - inspired by
         # https://stackoverflow.com/a/38223253
         while True:
             try:
                 line = input()
             except EOFError:
                 break
-            plaintext.append(line)
+            plaintext += line
+            plaintext += "\n"
 
     elif cmd == "2":
-        method_index = None
-        while method_index is None:
+        while True:
             print(f"Choose a garbling method by typing its number.")
             print("")
 
@@ -211,35 +210,54 @@ or return to the main menu by entering {len(GARBLE_METHODS)+2}.
                     print(
                         f"Option not recognized - please enter an option number between 1 and {len(GARBLE_METHODS)+2}."
                     )
-                    method_index = None
                 elif method_index == len(GARBLE_METHODS) + 1:
                     show_help()
-                    method_index = None
                 elif method_index == len(GARBLE_METHODS) + 2:
                     break
                 else:
                     garble_method = method_index - 1
                     update_garble_status()
                     print("Success!")
+                    break
     elif cmd == "3":
+        output_file = "test_file.txt"
         update_garble_status()
     elif cmd == "4":
         if garble_ready:
+            tokens = nltk.word_tokenize(plaintext, preserve_line=True)
             try:
-                subprocess.run(
-                    "python3", GARBLE_METHODS[garble_method], stdin=plaintext
+                # print(GARBLE_METHODS.values()[garble_method])
+                # print(json.dumps(tokens))
+                result = subprocess.run(
+                    ["python3", GARBLE_METHODS.values()[garble_method]],
+                    input=json.dumps(tokens),
+                    encoding="ascii",
+                    stdout=subprocess.PIPE,
                 )
-                clear_garble_status()
+                result = json.loads(result.stdout)
             except Exception as e:
                 print(
                     "Failed to garble! Settings have been preserved. This was the encountered error:"
                 )
                 print(repr(e), sys.stderr)
+                continue
+            if output_file is not None:
+                with open(output_file, "w") as f:
+                    for token in result:
+                        f.write(token)
+                        f.write(" ")
+                # clear_garble_status()
+            else:
+                for token in result:
+                    print(token, sep=" ")
+                print("")
+            # clear_garble_status()
+            print("Success!")
         else:
             print(
                 """
-                Can't choose this option yet! Check that you have added input text, chosen a
-                garbling method, and added an output method!
+Can't choose this option yet! Check that you have added input text, chosen a
+garbling method, and added an output method!
                 """
             )
 
@@ -250,7 +268,7 @@ or return to the main menu by entering {len(GARBLE_METHODS)+2}.
     else:
         print(
             """
-            Command not recognized - please select a menu option by entering a number
-            between 1 and 6.
+Command not recognized - please select a menu option by entering a number
+between 1 and 6.
             """
         )
