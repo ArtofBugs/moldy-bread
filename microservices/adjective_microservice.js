9f3ac906a3dc08a50635fa2fbb8246308b76dd11
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2024-12-03 01:38:42 -0800

    Adjective microservice - body version
    This doesn't work :)))
    Going to switch to queries instead to make it easier, even though I'll
    end up with super long URLs... honestly I don't really care.

---
+++ microservices/adjective_microservice.js
@@ -0,0 +1,40 @@
+// import express from 'express'
+const express = require('express')
+
+const app = express()
+const port = 8001
+
+const datamuseRoot = 'https://www.datamuse.com/api/words'
+const datamuseAdj = 'rel_jjb'
+
+app.post('/', async (req, res) => {
+  console.log('body')
+  console.log(req.body)
+  if (!req.body) {
+    res.status(200).send({})
+    return
+  }
+  let datamuseURL = new URL(datamuseRoot)
+  let substitutions = []
+  req.body.forEach(async (word) => {
+    datamuseURL.searchParams.append(datamuseAdj, word)
+    const datamuseFetch = await fetch(datamuseURL.href)
+    let result = await datamuseFetch.json()
+    if (result.status != 200) {
+      res.status(result.status).send(substitutions)
+      return
+    }
+    if (result.length == 0) {
+      substitutions.append(word)
+    } else {
+      substitutions.append(result[0].word)
+    }
+  })
+  console.log('substitutions')
+  console.log(substitutions)
+  res.status(200).send(substitutions)
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}.`)
+})
